51b3fc195779d5d9bf962f6c0dfc40c196352a29
sato_issei <sato.issei@alhinc.jp>
2025-11-26 17:23:07 +0900

    プロジェクトを共有

---
+++ src/main/java/com/example/nya_shopping/NyaShoppingApplication.java
@@ -0,0 +1,13 @@
+package com.example.nya_shopping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NyaShoppingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NyaShoppingApplication.class, args);
+	}
+
+}
