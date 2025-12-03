a6292bd83318b85344613db6bd1662a88920b5ab
sato_naoki <sato.naoki@alhinc.jp>
2025-12-03 15:19:01 +0900

    注文詳細画面（途中）

---
+++ src/main/java/com/example/nya_shopping/controller/form/OrderStatusForm.java
@@ -0,0 +1,11 @@
+package com.example.nya_shopping.controller.form;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class OrderStatusForm {
+    private Integer id;
+    private String status;
+}
