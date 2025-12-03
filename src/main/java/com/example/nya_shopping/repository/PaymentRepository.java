9b27159b457c6a1c4816eb5f7ce98046d2538895
taguchi.keita <taguchi.keita@alhinc.jp>
2025-12-03 17:05:58 +0900

    購入時通知実装

--- src/main/java/com/example/nya_shopping/repository/PaymentRepository.java
+++ src/main/java/com/example/nya_shopping/repository/PaymentRepository.java
@@ -1,7 +1,6 @@
 package com.example.nya_shopping.repository;
 
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface PaymentRepository {
