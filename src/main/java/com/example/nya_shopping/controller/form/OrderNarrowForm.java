c666161191bc52730bb982ce85bc0117704f9662
sato_naoki <sato.naoki@alhinc.jp>
2025-12-10 16:02:49 +0900

    日付値保持

--- src/main/java/com/example/nya_shopping/controller/form/OrderNarrowForm.java
+++ src/main/java/com/example/nya_shopping/controller/form/OrderNarrowForm.java
@@ -2,6 +2,7 @@ package com.example.nya_shopping.controller.form;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.sql.Timestamp;
 import java.time.LocalDate;
@@ -17,7 +18,9 @@ public class OrderNarrowForm {
     private String customerName;
     private String customerAddress;
     private String customerPhone;
+    @DateTimeFormat(pattern = "yyyy-MM-dd") // 値保持
     private LocalDate startDate;
+    @DateTimeFormat(pattern = "yyyy-MM-dd") //　値保持
     private LocalDate endDate;
     private String status;
     private Timestamp startTimeStamp;
